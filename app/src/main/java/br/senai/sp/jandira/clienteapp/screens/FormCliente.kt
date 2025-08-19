93995954fc89e69b29379c7a26886276d98634b5
GABRIEL SOUZA COSTA <24122537@dsn127m1246732.cfp127aluno.local>
2025-08-19 10:38:54 -0300

    codigo atualizado

--- app/src/main/java/br/senai/sp/jandira/clienteapp/screens/FormCliente.kt
+++ app/src/main/java/br/senai/sp/jandira/clienteapp/screens/FormCliente.kt
@@ -1,14 +1,19 @@
 package br.senai.sp.jandira.clienteapp.screens
 
+import android.content.ClipData.Item
 import android.util.Patterns
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +21,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.clienteapp.model.Cliente
@@ -48,6 +55,11 @@ fun formCliente(modifier: Modifier = Modifier) {
         return !isNomeError && !isEmailError
     }
 
+    // Variavel que vai exibir a caixa de diálogo
+    val mostrarTelaSucesso by remember {
+        mutableStateOf(false)
+    }
+
     // Criar uma instância do RetrofitFactory
     val clienteApi = RetrofitFactory().getClienteService()
 
@@ -56,6 +68,17 @@ fun formCliente(modifier: Modifier = Modifier) {
             .padding(16.dp)
             .fillMaxSize()
     ) {
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Ícone do cadastro"
+            )
+            Text(
+                text = "Novo Cliente",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+        }
         OutlinedTextField(
             value = nomeCliente,
             onValueChange = { nome ->
@@ -120,6 +143,20 @@ fun formCliente(modifier: Modifier = Modifier) {
                 .fillMaxWidth()
         ) {
             Text(text = "Gravar Cliente")
+        }
+        if (mostrarTelaSucesso){
+            AlertDialog(
+                onDismissRequest = {},
+                title = { Text(text = "Sucesso") },
+                text = { Text(text = "Cliente gravado com sucesso!") },
+                confirmButton = {
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text(text = "OK")
+                    }
+                }
+            )
         }
     }
 }
